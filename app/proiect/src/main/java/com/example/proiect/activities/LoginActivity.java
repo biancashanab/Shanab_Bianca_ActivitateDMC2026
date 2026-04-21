276e8a79c23fe83983130bdddf7daf162e586947
Shanab Bianca <161047767+biancashanab@users.noreply.github.com>
2026-04-21 13:10:27 +0300

    first commit for project

---
+++ app/proiect/src/main/java/com/example/proiect/activities/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.proiect.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.proiect.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        
+        // Logica de navigare catre Dashboard sau Register va fi adaugata aici
+    }
+}
---
+++ app/proiect/src/main/java/com/example/proiect/activities/PaperDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.example.proiect.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.proiect.R;
+
+public class PaperDetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paper_details);
+    }
+}
